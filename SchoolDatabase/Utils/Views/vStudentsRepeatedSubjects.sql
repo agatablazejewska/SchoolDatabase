e417be077e5074873fd6b7eb73d25ad8c61b58b5
Agata <agata.blazejewska0@gmail.com>
2018-10-11 12:44:56 +0200

    Added view vStudentsRepeatedSubjects

---
+++ SchoolDatabase/Utils/Views/vStudentsRepeatedSubjects.sql
@@ -0,0 +1,4 @@
+﻿CREATE VIEW [utils].[vStudentsRepeatedSubjects]
+	AS SELECT srs.RepeatingStudentId, srs.RepeatedSubjectId, ss.SchoolSubjectName, srs.Paid FROM listeners.StudentsRepeatedSubjects AS srs
+	INNER JOIN studies.SchoolSubjects AS ss
+	ON srs.RepeatedSubjectId = ss.SchoolSubjectId
